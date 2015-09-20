86ca4778f72f66c36a7657376ddb08714c0c3de9
SatoshiShimada <mylinux1204@gmail.com>
2015-09-21 00:41:53 +0900

    Support Japanese

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 
 #include <QApplication>
+#include <QTranslator>
 
 #include "image_editor.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+
+	// Translator
+	QTranslator trans;
+	trans.load("ja.qm", qApp->applicationDirPath());
+	app.installTranslator(&trans);
 
 	ImageEditor imageEditor;
 
